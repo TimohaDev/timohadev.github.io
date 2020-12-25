0255b129b4f73a986c8b10a37639d2fcc954c2d5
t1mosha <pytimdev@gmail.com>
2020-12-25 13:42:19 +0200

    Add NewYear theme 🎄

---
+++ js/description.js
@@ -0,0 +1,10 @@
+
+	var t = new Typed('#description', {
+		strings: ['Timothy', '14 years old, grade 9', 'I write bots, sites, I also do arduino', 'love coding', 'Слава Україні!'],
+		typeSpeed: 100,
+		backSpeed: 0,
+		cursorChar: '|',
+		smartBackspace: true,
+		fadeOut: true,
+		loop: true
+	});
